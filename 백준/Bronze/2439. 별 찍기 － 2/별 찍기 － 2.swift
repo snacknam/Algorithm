e1b3729eaa8425c6bd6c0c6cda449c17ee521e4a
Kwansik Nam <110089773+snacknam@users.noreply.github.com>
2023-09-08 12:54:21 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 12 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.swift
@@ -0,0 +1,10 @@
+let input = Int(readLine()!)!
+for i in 1...input {
+    for _ in 0..<input-i {
+        print(" ", terminator: "")
+    }
+    for _ in 0..<i {
+        print("*", terminator: "")
+    }
+    print()
+}
