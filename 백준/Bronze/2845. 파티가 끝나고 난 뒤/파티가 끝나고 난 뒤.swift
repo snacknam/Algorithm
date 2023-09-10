b7a4469028e3e665e9fa94e690ee7394160b2444
Kwansik Nam <110089773+snacknam@users.noreply.github.com>
2023-09-11 02:58:47 +0900

    [Bronze IV] Title: 파티가 끝나고 난 뒤, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.swift
+++ 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.swift
@@ -3,9 +3,5 @@ let partyPeople = input[0] * input[1]
 
 let news = readLine()!.split{$0 == " "}.map{Int($0)!}
 for i in 0..<5 {
-    if i == 4 {
-        print("\(news[i] - partyPeople)", terminator: "")
-    } else {
-        print("\(news[i] - partyPeople)", terminator: " ")
-    }
+    print("\(news[i] - partyPeople)", terminator: " ")
 }
