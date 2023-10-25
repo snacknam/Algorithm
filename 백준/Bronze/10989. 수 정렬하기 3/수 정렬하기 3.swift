ad6a1ac0cbef6e407411f42dd29073618f881f19
Kwansik Nam <110089773+snacknam@users.noreply.github.com>
2023-10-25 10:38:56 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1400 ms, Memory: 263604 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.swift
@@ -0,0 +1,47 @@
+import Foundation
+
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+}
+
+let file = FileIO()
+let input = file.readInt()
+var arr = [Int](repeating: 0, count: 10001)
+var result = ""
+
+for _ in 0..<input {
+    let n = file.readInt()
+    arr[n] += 1
+}
+for i in 1...10000 {
+    result += String(repeating: "\(i)\n", count: arr[i])
+}
+print(result)
